3c5c655ec1d3b0debf90730d00a924eb1e6bf482
mohlcips <nick@kiwi-hacker.net>
2021-04-27 21:18:08 +0000

    change of URL

--- src/get_ftse.sh
+++ src/get_ftse.sh
@@ -2,6 +2,10 @@
 
 #lazy get of FTSE 
 
-w3m -dump "https://www.google.com/search?q=ftse100" | grep -B1 "UKX(INDEXFTSE)" | head -n 1 | awk '{gsub(/,/,""); print $1}' > /data/ftse100.log
+# google now asks for cookies
+#w3m -dump "https://www.google.com/search?q=ftse100" | grep -B1 "UKX(INDEXFTSE)" | head -n 1 | awk '{gsub(/,/,""); print $1}' | tee /data/ftse100.log
+#w3m -dump "https://www.google.com/search?q=ftse250" | grep -B1 "MCX(INDEXFTSE)" | head -n 1 | awk '{gsub(/,/,""); print $1}' | tee /data/ftse250.log
 
-w3m -dump "https://www.google.com/search?q=ftse250" | grep -B1 "MCX(INDEXFTSE)" | head -n 1 | awk '{gsub(/,/,""); print $1}' > /data/ftse250.log
+w3m -dump "https://www.londonstockexchange.com/indices/ftse-100" | grep -A1 "Index value" | tail -n 1 | awk '{gsub(/,/,""); print $1}' | tee /data/ftse100.log
+
+w3m -dump "https://www.londonstockexchange.com/indices/ftse-250" | grep -A1 "Index value" | tail -n 1 | awk '{gsub(/,/,""); print $1}' | tee /data/ftse250.log
